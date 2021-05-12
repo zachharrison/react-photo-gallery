78e88969290c7231e80730817ed23e9aabcf2cbd
Zach Harrison <zharrison@shaw.ca>
2021-05-12 13:36:35 -0700

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyA3E-cZ7zobd54V3KsWbyO5-e-Puifx-24',
+  authDomain: 'firegram-e1119.firebaseapp.com',
+  projectId: 'firegram-e1119',
+  storageBucket: 'firegram-e1119.appspot.com',
+  messagingSenderId: '954141924685',
+  appId: '1:954141924685:web:dd0b0d9120939948046b04',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage();
+const firestore = firebase.firestore();
+
+export { storage, firestore };
