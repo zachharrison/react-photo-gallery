8f6f9d4add908b0a34a3b65bf3f0ee987e92dce0
Zach Harrison <zharrison@shaw.ca>
2021-05-12 13:19:13 -0700

    Added Header and base styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/Header';
+
 const App = () => {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <Header />
     </div>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='title'>
+      <h1>FireGram</h1>
+      <h2>Your Photos</h2>
+      <p>Add some new photos to your amazing gallery.</p>
+    </div>
+  );
+};
+
+export default Header;
