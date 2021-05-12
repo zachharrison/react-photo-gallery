2a3c04787bbe8a2ccbbf4c11843ee29882f9dac7
Zach Harrison <zharrison@shaw.ca>
2021-05-12 13:50:35 -0700

    Add UploadForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import UploadForm from './components/UploadForm';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
+      <UploadForm />
     </div>
   );
 };
