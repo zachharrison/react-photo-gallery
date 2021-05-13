8d6add03996b01c86a44acbed39554fd91ad9e84
Zach Harrison <zharrison@shaw.ca>
2021-05-12 18:43:45 -0700

    Added modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
 import UploadForm from './components/UploadForm';
 
 const App = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className='App'>
       <Header />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 };
